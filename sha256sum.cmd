8ad41bdec204e6f6e148d4716f7277118179b6d4
Michael R. Taylor <michael@atomic.city>
2017-12-09 22:24:44 -0800

    Added sha256sum command

---
+++ sha256sum.cmd
@@ -0,0 +1,2 @@
+@echo off
+node sha256sum %*
---
+++ sha256sum.js
@@ -0,0 +1,39 @@
+const checksums = require('./checksums.js');
+const progVersion = require('./package.json').version;
+const hashType = 'sha256';
+
+const program = require('commander');
+
+program
+    .version(progVersion, '-v, --version')
+    .description('Compute and check SHA-256 sums')
+    .usage('[OPTION...] <FILE...>')
+    .option('-c, --check', 'read SHA-256 sums from the FILEs and check them')
+    .parse(process.argv);
+
+// Output usage info if no arguments were given
+if (!program.args.length) program.help();
+
+const files = program.args;
+
+if (program.check) {
+    // Check the given SHA-256 check files
+    checksums.hashCheck(hashType, files, (err, fileName, expectedHash, actualHash) => {
+        if (err) {
+            console.log(err)
+        } else if (actualHash === expectedHash) {
+            console.log(fileName, 'OK')
+        } else {
+            console.log(fileName, 'FAILED')
+        }
+    })
+} else {
+    // Perform SHA-256 sums on the given files
+    checksums.hash(hashType, files, (err, hash) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(hash.hash, '*' + hash.fileName)
+        }
+    })
+}
